b167a502ae4f59689e77628ae4364fbce0ae8f66
Malcolm McSwain <mcswain1@pacbell.net>
2017-03-17 12:07:40 -0700

    Fixed indenting with new atom tabs #3

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ router.set('view engine', 'pug');
 router.set('views', `${__dirname}/templates`);
 
 router.get('/', (req, res, next) => {
-   res.render('index', { title: 'Home', name: 'Malcolm' });
-   next();
+  res.render('index', { title: 'Home' });
+  next();
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', () => {
-    console.log('Server running!');
+  console.log('Server running!');
 });
