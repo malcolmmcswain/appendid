1696cde09057e08e94b809591005bca987fb4093
Malcolm McSwain <mcswain1@pacbell.net>
2017-03-17 12:04:59 -0700

    Routed initial demo #1 #2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express'),
        router = express(),
        server = http.createServer(router);
 
-router.use(express.static(`${__dirname}/public`));
+router.use(express.static(`${__dirname}/public/docs`));
 
 router.set('view engine', 'pug');
 router.set('views', `${__dirname}/templates`);
